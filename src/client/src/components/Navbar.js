4902184d2ab55668e6a6d8e5059c82d0ac48b19c
Tomáš Janíček <tomasjanicek221@gmail.com>
2020-09-10 16:25:19 +0200

    Add links to header

--- src/client/src/components/Navbar.js
+++ src/client/src/components/Navbar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import {useAuth0} from "@auth0/auth0-react";
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
     const {loginWithRedirect, isAuthenticated, logout} = useAuth0();
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
-            <span className="navbar-brand">Timesheet</span>
+            <Link to="/" className="navbar-brand">Timesheet</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"/>
             </button>
             <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/app" className="nav-link btn btn-outline-primary">App</Link>
+                    </li>
+
                     {isAuthenticated ? (
                         <li className="nav-item">
                             <button className="nav-link btn btn-outline-primary" onClick={logout}>Log Out</button>
@@ -25,9 +30,7 @@ const Navbar = () => {
                     )}
                 </ul>
             </div>
-
         </nav>
-
     )
 }
 
